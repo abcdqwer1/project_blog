581833ff9ef8be1064a8f8c2ac2781e217f7c0e4
LeeCH <ckdgus2323@naver.com>
2023-11-08 02:46:55 +0900

    FIX: media, static urlpatterns

--- projectblog/urls.py
+++ projectblog/urls.py
@@ -2,13 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('blog/', include('blog.urls')),
     path('accounts/', include('accounts.urls')),
-    # path('commnets/', include('commnets.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+urlpatterns += re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
