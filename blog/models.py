ea27b4a3c053bd2e3258610335d6601367cb0e0d
LeeCH <ckdgus2323@naver.com>
2023-11-12 14:42:32 +0900

    FEAT: recommnet

--- blog/models.py
+++ blog/models.py
@@ -37,6 +37,21 @@ class Comment(models.Model):
     def __str__(self):
         return self.message
     
+class ReComment(BaseDateTime):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments'
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    
+    def __str__(self):
+        return self.message
+    
+    
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
     def __str__(self):
